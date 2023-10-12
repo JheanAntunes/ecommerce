3c2c864c649f3c3dfb83bfbc3f745474cf7e87a5
jhean <brizadaojhean@gmail.com>
2023-10-11 23:56:15 -0300

    add media query no elemento main padrão

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
             <body className={roboto.className}>
                 <ProviderGlobal>
                     <HeaderRaiz />
-                    <main>{children}</main>
+                    <main className="grid @xs:grid-cols-layoutGridXs @sm:grid-cols-layoutGridSm @md:grid-cols-layoutGridMd @lg:grid-cols-layoutGridLg @xl:grid-cols-layoutGridXl @2xl:grid-cols-layoutGrid2xl @3xl:grid-cols-layoutGrid3xl @4xl:grid-cols-layoutGrid4xl @5xl:grid-cols-layoutGrid5xl @6xl:grid-cols-layoutGrid6xl @7xl:grid-cols-layoutGrid7xl">
+                        {children}
+                    </main>
                 </ProviderGlobal>
             </body>
         </html>
