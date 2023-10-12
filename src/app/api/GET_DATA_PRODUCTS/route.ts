9b0e7797f3f5eb87c70b123be5379ab58a777319
jhean <brizadaojhean@gmail.com>
2023-10-11 23:52:47 -0300

    Criação da api para pegar produtos

---
+++ src/app/api/GET_DATA_PRODUCTS/route.ts
@@ -0,0 +1,16 @@
+import { schema_Api_Data, Type_Api_DATA } from '@/schema/api/schema_Api_data'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+    try {
+        const response = await fetch('https://dummyjson.com/products?limit=8')
+        if (!response.ok) throw new Error('ERROR:GET_PRODUCTS')
+        const data: Type_Api_DATA = await response.json()
+        const validatedData = schema_Api_Data.safeParse(data)
+        if (!validatedData.success)
+            throw new Error('ERROR: ', validatedData.error)
+        return NextResponse.json(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
