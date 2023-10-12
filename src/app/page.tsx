7d0cdd3b47c7dd5ef3dd433b03223b940584b617
jhean <brizadaojhean@gmail.com>
2023-10-11 23:56:44 -0300

    Import carrousel + suspense do data carrousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import Slider_Server_GET_DADOS from '@/components/slider/Slider_Server_GET_DADOS'
+import { Suspense } from 'react'
+
 export default function Home() {
     return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-            eveniet. Architecto, facere quia. Suscipit veniam temporibus qui
-            minima? Corrupti, officiis repudiandae voluptatem dolor iure autem
-            tempora amet hic iusto aperiam! Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Temporibus perspiciatis, corrupti
-            libero vitae accusamus maiores nihil ullam incidunt, aspernatur iste
-            nulla quaerat quae? Magnam, quidem odio optio minus ullam suscipit.
-        </div>
+        <>
+            <Suspense fallback={<p>Carregando dados...</p>}>
+                {/* @ts-ignore */}
+                <Slider_Server_GET_DADOS />
+            </Suspense>
+        </>
     )
 }
