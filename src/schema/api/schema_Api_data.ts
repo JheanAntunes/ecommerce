1ac8b848c2140b2ba0493817aa7967514e0a6d15
jhean <brizadaojhean@gmail.com>
2023-10-11 23:53:04 -0300

    schema para validar os dados da api de produtos

---
+++ src/schema/api/schema_Api_data.ts
@@ -0,0 +1,24 @@
+import * as z from 'zod'
+export const Object_Zod_Product = z.object({
+    id: z.number(),
+    title: z.string(),
+    description: z.string(),
+    price: z.number(),
+    discountPercentage: z.number(),
+    rating: z.number(),
+    stock: z.number(),
+    brand: z.string(),
+    category: z.string(),
+    thumbnail: z.string(),
+    images: z.string().array(),
+})
+
+export const schema_Api_Data = z.object({
+    products: Object_Zod_Product.array(),
+    total: z.number(),
+    skip: z.number(),
+    limit: z.number(),
+})
+
+export type Type_Api_DATA = z.infer<typeof schema_Api_Data>
+export type Type_Api_Product = z.infer<typeof Object_Zod_Product>
