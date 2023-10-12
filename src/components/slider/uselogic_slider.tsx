b12ec4dc9d0182911578285d88f0484781ecd5bf
jhean <brizadaojhean@gmail.com>
2023-10-12 20:00:06 -0300

    nova funcionalidade no slider! AutoSlider

--- src/components/slider/uselogic_slider.tsx
+++ src/components/slider/uselogic_slider.tsx
@@ -1,27 +1,50 @@
-import { useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 function Use_logic_slider(Images: string[]) {
     const [currentImg, setCurrentImg] = useState(0)
     const slide_Direction_Animation = useRef<'left' | 'right'>('left')
+    const limit_Array_Images_Lenght = useRef<number>(Images.length)
+    const idTimout = useRef<NodeJS.Timeout | undefined>(undefined)
+    const timerTimout = 3500 //3segundo e meio
     //Funções auxiliares Slider.
     const handle_Click_Button_Left_Slider_Previous = () => {
         setCurrentImg((imgAtual) =>
-            imgAtual - 1 < 0 ? Images.length - 1 : imgAtual - 1
+            imgAtual - 1 < 0
+                ? limit_Array_Images_Lenght.current - 1
+                : imgAtual - 1
         )
         slide_Direction_Animation.current = 'left'
     }
 
-    const handle_Click_Button_Right_Slider_Next = () => {
+    const handle_Click_Button_Right_Slider_Next = useCallback(() => {
         setCurrentImg((imgAtual) =>
-            imgAtual + 1 === Images.length ? 0 : imgAtual + 1
+            imgAtual + 1 === limit_Array_Images_Lenght.current
+                ? 0
+                : imgAtual + 1
         )
         slide_Direction_Animation.current = 'right'
-    }
+    }, [])
 
     const handle_Click_Buttons_Dots_Slider = (dot: number) => {
         setCurrentImg(dot)
         slide_Direction_Animation.current = dot > currentImg ? 'right' : 'left'
     }
+
+    // const autoSlider = () => {
+    //     if(idTimout)
+    //     const idTimout = setTimeout(handle_Click_Button_Right_Slider_Next, 1000)
+    // }
+    useEffect(() => {
+        if (idTimout.current) clearTimeout(idTimout.current)
+        idTimout.current = setTimeout(
+            handle_Click_Button_Right_Slider_Next,
+            timerTimout
+        )
+
+        return () => {
+            clearTimeout(idTimout.current)
+        }
+    }, [currentImg, handle_Click_Button_Right_Slider_Next])
 
     return {
         currentImg,
