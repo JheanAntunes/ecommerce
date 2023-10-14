13b42ccc206caf2ee7d0af37a6d87d463e5b3a80
jhean <brizadaojhean@gmail.com>
2023-10-14 19:05:14 -0300

    renomeação de pastas e atualizando imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Categoria from '@/components/categoria/categoria'
-import Mais_Vendidos_Produtos from '@/components/mais-vendidos/Mais_Vendidos_Produtos'
+import Categoria from '@/components/section-categoria/categoria'
+import Mais_Vendidos_Produtos from '@/components/section-mais-vendidos/Mais_Vendidos_Produtos'
 import SkeletonSlider from '@/components/skeletons/slider/skeletonSlider'
 import Slider_Server_GET_DADOS from '@/components/slider/Slider_Server_GET_DADOS'
 import { Suspense } from 'react'
