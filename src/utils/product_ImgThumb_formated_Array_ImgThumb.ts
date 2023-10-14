419fcc6984003e542067124450edc80874699484
jhean <brizadaojhean@gmail.com>
2023-10-14 18:58:33 -0300

    Incremento-propriedade nova para reutilizar codigo

--- src/utils/product_ImgThumb_formated_Array_ImgThumb.ts
+++ src/utils/product_ImgThumb_formated_Array_ImgThumb.ts
@@ -1,13 +1,14 @@
 import { Type_Data_Image_Description } from '@/components/slider/Type_data_Image_Description'
 import { Type_Api_DATA } from '@/schema/api/schema_Api_data'
 
-export const product_ImgThumb_Description_formated_Array = (
+export const product_ImgThumb_Description_Title_formated_Array = (
     data: Type_Api_DATA
 ): Type_Data_Image_Description[] => {
     const dataImageDescription = data.products.map((product) => {
         return {
             image_thumbnail: product.thumbnail,
             description: product.description,
+            title: product.title,
         }
     })
     return dataImageDescription
