9b1662c5bb9b061fa448dd792bfadd2fab3fa01b
jhean <brizadaojhean@gmail.com>
2023-10-17 18:45:03 -0200

    import: section de produtos.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Section_Categoria from '@/components/section-categoria/section_Categoria'
 import Section_Mais_Vendidos_Produtos from '@/components/section-mais-vendidos/section_Mais_Vendidos_Produtos'
-import Section_Produtos from '@/components/section-produtos/section_Produtos'
+import Section_Card_Produto_GET_DADOS from '@/components/section-produtos/section_produto_GET_DADOS'
+
 import SkeletonSlider from '@/components/skeletons/slider/skeletonSlider'
 import Slider_Server_GET_DADOS from '@/components/slider/Slider_Server_GET_DADOS'
 import { Suspense } from 'react'
@@ -14,8 +15,10 @@ export default function Home() {
             </Suspense>
             <Section_Categoria />
             <Section_Mais_Vendidos_Produtos />
-            <Section_Produtos />
-            <div className="h-[800px]">dasdsa</div>
+            <Suspense fallback={<p>carregando...</p>}>
+                {/* @ts-ignore */}
+                <Section_Card_Produto_GET_DADOS />
+            </Suspense>
         </>
     )
 }
