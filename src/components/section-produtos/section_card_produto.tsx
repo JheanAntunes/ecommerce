1f4c9d639dea1784892ebc864fae522c27384fa8
jhean <brizadaojhean@gmail.com>
2023-10-17 18:42:02 -0200

    Comp: Interface de produto.

---
+++ src/components/section-produtos/section_card_produto.tsx
@@ -0,0 +1,67 @@
+import { Poppins } from 'next/font/google'
+import Image from 'next/image'
+import { Type_Data_Image_Description } from '../slider/Type_data_Image_Description'
+import { TypographyH3, TypographySmall } from '../Typography/typography'
+import Section_card_product_like from './section_card_product_like'
+
+const poppins = Poppins({
+    weight: ['400', '500'],
+    subsets: ['latin'],
+    fallback: ['sans-serif', 'serif'],
+})
+type Type_Section_Card_Produto_Props = {
+    data_Image_Description_Title: Type_Data_Image_Description[]
+}
+
+function Section_Card_Produto({
+    data_Image_Description_Title,
+}: Type_Section_Card_Produto_Props) {
+    return (
+        <div className="grid grid-cols-4 gap-5 @xs:grid-cols-1 @md:grid-cols-2 @xl:grid-cols-3 @4xl:grid-cols-4">
+            {data_Image_Description_Title.map(
+                ({ description, image_thumbnail, title, price, id }, index) => (
+                    <div
+                        key={description + title}
+                        className="rounded bg-slate-50 p-5 shadow"
+                    >
+                        <div className="relative w-full">
+                            <div className="relative h-32 w-full overflow-hidden">
+                                {/* Image */}
+                                <Image
+                                    src={image_thumbnail}
+                                    alt={description}
+                                    className="object-contain"
+                                    fill
+                                    sizes="100vw"
+                                />
+                            </div>
+                            <div className="absolute right-0 top-0 -translate-x-1 -translate-y-2">
+                                {/* Headt - Gostei Produto */}
+                                <Section_card_product_like
+                                    index={index}
+                                    id_product={id}
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <TypographyH3
+                                className={`${poppins.className} text-xl font-normal text-slate-800`}
+                            >
+                                {title}
+                            </TypographyH3>
+                            <p className="flex items-center gap-1 text-slate-600">
+                                <span className="sr-only">Preço</span>
+                                <TypographySmall>R$</TypographySmall>
+                                <span className={`text-slate-800`}>
+                                    {price}
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                )
+            )}
+        </div>
+    )
+}
+
+export default Section_Card_Produto
