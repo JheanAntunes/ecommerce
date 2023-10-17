28da0b12f93b4b2c3d9d8fb19924c6783cf40e09
jhean <brizadaojhean@gmail.com>
2023-10-17 21:37:03 -0200

    Comp: Wrapper do nav-heart-qtd

---
+++ src/components/layout/nav/nav-icon-heart-qtd.tsx
@@ -0,0 +1,11 @@
+import NavHeartQtd from './nav-heart-qtd'
+
+function NavIconHeartQtd() {
+    return (
+        <div className="relative flex items-center justify-center">
+            <NavHeartQtd />
+        </div>
+    )
+}
+
+export default NavIconHeartQtd
