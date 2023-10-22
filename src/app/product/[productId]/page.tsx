6e849a92ac278562d1ddd5dc24026d4d365876fe
jhean <brizadaojhean@gmail.com>
2023-10-21 23:37:30 -0200

    refactor: reutilização de código

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -2,7 +2,9 @@ import Product_Detalhes from '@/components/product-dinamico/product-detalhes/pro
 import Product_Images from '@/components/product-dinamico/product-images/product_images'
 import Localidade_Pathname from '@/components/product-dinamico/product-localidade-pathname/localidade_pathname'
 import Product_Qtd_Forma_Pagamento from '@/components/product-dinamico/product-qtd-forma-pagamento/product_qtd_forma_pagamento'
+import { TypographySmall } from '@/components/Typography/typography'
 import { Type_Api_Product } from '@/schema/api/schema_Api_data'
+import { ChevronRight } from 'lucide-react'
 import { Metadata, ResolvingMetadata } from 'next'
 
 export type Type_variavel_url = string | string[] | undefined
@@ -52,7 +54,10 @@ async function PageProductDynamic({
 
     return (
         <section className="col-start-2 col-end-2 mt-5">
-            <Localidade_Pathname titleProduct={product.title} />
+            <Localidade_Pathname titleProduct={product.title}>
+                <ChevronRight width={20} height={20} />
+                <TypographySmall>Produtos</TypographySmall>
+            </Localidade_Pathname>
             <div className="grid grid-cols-1 gap-5 @2xl:grid-cols-[380px_1fr] @3xl:grid-cols-[380px_1fr] @4xl:grid-cols-[440px_1fr] @5xl:grid-cols-[480px_2fr_1fr]">
                 <Product_Images
                     image_thumbnail={product.thumbnail}
