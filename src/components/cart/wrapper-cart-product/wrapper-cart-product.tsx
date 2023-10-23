f0dfe7edfbbb18f98634598a019b71c29bd1eefc
jhean <brizadaojhean@gmail.com>
2023-10-23 20:51:49 -0200

    feat: container do card produto-get-datae-map-card

--- src/components/cart/wrapper-cart-product/wrapper-cart-product.tsx
+++ src/components/cart/wrapper-cart-product/wrapper-cart-product.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { formated_CartDetails_Object_value } from '@/utils/formated_CartDetails_Object_value'
+import { PropsWithChildren } from 'react'
 import { useShoppingCart } from 'use-shopping-cart'
 import Cart_Product_Card from '../cart_product_card'
 
-function WrapperCartProduct() {
+function WrapperCartProduct({ children }: PropsWithChildren) {
     const { cartDetails } = useShoppingCart()
 
     const array_Cart_Product = formated_CartDetails_Object_value(cartDetails)
+
+    if (array_Cart_Product.length === 0) return <>{children}</>
 
     return (
         <div className="flex flex-col gap-5">
