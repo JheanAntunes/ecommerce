2288a79b50a478ab036f4a6bbb4d5788fcd866bf
jhean <brizadaojhean@gmail.com>
2023-10-23 20:48:29 -0200

    feat: Página do carrinho + interface

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,6 @@
 import CartAside from '@/components/cart/aside/cart-aside'
+import { CardAsideMobile } from '@/components/cart/aside/cart-aside-mobile'
+import NotFoundProductCart from '@/components/cart/not-found-product-cart/notFoundProductCart'
 import WrapperCartProduct from '@/components/cart/wrapper-cart-product/wrapper-cart-product'
 import Wrapper_H1_Button_Clear_Cart from '@/components/cart/wrapper_h1_button_clear_cart'
 import Localidade_Pathname from '@/components/product-dinamico/product-localidade-pathname/localidade_pathname'
@@ -6,7 +8,7 @@ import { ChevronRight } from 'lucide-react'
 
 function PageCart() {
     return (
-        <section className="col-start-2 col-end-2 mt-5 flex flex-col gap-5">
+        <section className="col-start-2 col-end-2 mt-5 flex h-full w-full flex-col gap-5">
             <Localidade_Pathname titleProduct="Carrinho">
                 <ChevronRight
                     className="text-slate-800"
@@ -15,10 +17,13 @@ function PageCart() {
                 />
             </Localidade_Pathname>
             <Wrapper_H1_Button_Clear_Cart />
-            <div className="grid grid-cols-[1fr_255px] gap-5">
-                <WrapperCartProduct />
+            <div className="grid grid-cols-1 gap-5 @4xl:grid-cols-[1fr_255px]">
+                <WrapperCartProduct>
+                    <NotFoundProductCart />
+                </WrapperCartProduct>
                 <CartAside />
             </div>
+            <CardAsideMobile />
         </section>
     )
 }
