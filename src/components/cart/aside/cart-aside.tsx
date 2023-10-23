06197a23ad4329b50c7347b7066458ec912b8889
jhean <brizadaojhean@gmail.com>
2023-10-23 20:49:56 -0200

    feat: Aside-lateral na página de carrinho.

--- src/components/cart/aside/cart-aside.tsx
+++ src/components/cart/aside/cart-aside.tsx
@@ -1,16 +1,14 @@
 import { TypographyDecoradorH4 } from '@/components/Typography/typography'
 import { Button } from '@/components/ui/button'
-import { Banknote } from 'lucide-react'
+import CartAsideButtonPagamento from './cart-aside-button-pagamento'
 import CartAsideDetalhes from './cart-aside-detalhes'
 
 function CartAside() {
     return (
-        <aside className="sticky top-0 z-20 flex h-72 flex-col gap-5 rounded-xl border p-5 shadow">
+        <aside className="sticky top-0 z-20 flex h-72 flex-col gap-5 rounded-xl border p-5 shadow    @4xl:flex">
             <TypographyDecoradorH4>Resumo</TypographyDecoradorH4>
             <CartAsideDetalhes />
-            <Button className="gap-2 bg-blue-500 hover:bg-blue-600">
-                Pagamento <Banknote />
-            </Button>
+            <CartAsideButtonPagamento />
             <Button variant="outline">Continuar Comprando</Button>
         </aside>
     )
