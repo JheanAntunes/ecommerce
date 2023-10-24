44bb0274b4540f1149c03bf2daa1e0f11eaade02
jhean <brizadaojhean@gmail.com>
2023-10-23 23:01:30 -0200

    refactor: interface desnecessaria.

--- src/components/product-dinamico/product-qtd-forma-pagamento/product_qtd_forma_pagamento.tsx
+++ src/components/product-dinamico/product-qtd-forma-pagamento/product_qtd_forma_pagamento.tsx
@@ -1,9 +1,4 @@
-import {
-    TypographyDecoradorH2,
-    TypographyDecoradorH3,
-} from '@/components/Typography/typography'
-import { Button } from '@/components/ui/button'
-import { Minus, Plus } from 'lucide-react'
+import { TypographyDecoradorH3 } from '@/components/Typography/typography'
 import Image from 'next/image'
 import SvgBoleto from '../../../svgs/boleto.svg'
 import SvgMasterCard from '../../../svgs/mastercard.svg'
@@ -11,28 +6,14 @@ import SvgPix from '../../../svgs/pix-106.svg'
 import SvgVisa from '../../../svgs/visa.svg'
 type Type_Product_Qtd_Forma_Pagamento_Props = {
     price: number
+    id: string
+    name: string
 }
 function Product_Qtd_Forma_Pagamento({
     price,
 }: Type_Product_Qtd_Forma_Pagamento_Props) {
     return (
-        <section className="mt-5 flex flex-col gap-5 @2xl:col-span-2 @5xl:col-span-1">
-            <TypographyDecoradorH2 className="text-lg text-slate-800 @5xl:text-2xl @5xl:before:h-6">
-                Quantidade
-            </TypographyDecoradorH2>
-            <div className="flex items-center justify-between">
-                <Button size="icon" variant="outline">
-                    <Plus />
-                </Button>
-                <span className="text-3xl text-slate-800">0</span>
-                <Button size="icon" variant="outline">
-                    <Minus />
-                </Button>
-            </div>
-            <div className="flex items-center justify-between">
-                <span className=" text-slate-900">Preço total</span>
-                <span className="text-slate-700">R$ {price}</span>
-            </div>
+        <div className="mt-5 flex flex-col gap-5 @2xl:col-span-2 @5xl:col-span-1">
             <section className="flex flex-col gap-5">
                 <TypographyDecoradorH3 className="text-base text-slate-800 @5xl:text-lg @5xl:before:h-[18px]">
                     Forma de pagamento
@@ -68,7 +49,7 @@ function Product_Qtd_Forma_Pagamento({
                     />
                 </div>
             </section>
-        </section>
+        </div>
     )
 }
 
