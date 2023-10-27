613479e0019aa3570fd456ebe0b44708d69abae9
jhean <brizadaojhean@gmail.com>
2023-10-27 19:33:51 -0200

    fix: dados de testes

--- src/utils/imgs_teste_slider/Images_Teste.ts
+++ src/utils/imgs_teste_slider/Images_Teste.ts
@@ -1,8 +1,11 @@
 import { Type_Data_Image_Description } from '@/components/slider/Type_data_Image_Description'
 
 const Object_Data_Teste_Simulation0: Type_Data_Image_Description = {
+    id: 0,
     image_thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
     description: 'Teste0',
+    title: 'abc',
+    price: 200,
 }
 const Object_Data_Teste_Simulation01: Type_Data_Image_Description = {
     ...Object_Data_Teste_Simulation0,
